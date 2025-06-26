4b777e476ef853a87bc6800d1c555f7abf82ed07
joselawis <joselawis@gmail.com>
2025-06-26 10:18:10 +0200

    better gender domain object

--- msvc-users/src/main/java/com/jlcm/code/challenge/msvc/users/domain/Gender.java
+++ msvc-users/src/main/java/com/jlcm/code/challenge/msvc/users/domain/Gender.java
@@ -1,5 +1,31 @@
 package com.jlcm.code.challenge.msvc.users.domain;
 
 public enum Gender {
-    MALE, FEMALE, OTHER
+    MALE("male", 0), FEMALE("female", 1), OTHER("other", 2);
+
+    private final String label;
+    private final int index;
+
+    Gender(String label, int index) {
+        this.label = label;
+        this.index = index;
+    }
+
+    public static Gender fromIndex(int index) {
+        for (Gender gender : Gender.values()) {
+            if (gender.index == index) {
+                return gender;
+            }
+        }
+        throw new IllegalArgumentException("Invalid index for Gender: " + index);
+    }
+
+    public static Gender fromLabel(String label) {
+        for (Gender gender : Gender.values()) {
+            if (label != null && label.equalsIgnoreCase(gender.label)) {
+                return gender;
+            }
+        }
+        throw new IllegalArgumentException("Invalid label for Gender: " + label);
+    }
 }
