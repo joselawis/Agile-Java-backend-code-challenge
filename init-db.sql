f7c382012ff104d12fb5cff16015eb0668e51df9
joselawis <joselawis@gmail.com>
2025-06-26 12:00:41 +0200

    database creation with docker compose

---
+++ init-db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    title VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    gender NUMERIC(1) NOT NULL,
+    picture_url VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    state_province VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL
+);
