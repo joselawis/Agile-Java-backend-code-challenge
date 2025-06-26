47faf99d814321d9c36d6fd95ee710e8af30e877
joselawis <joselawis@gmail.com>
2025-06-26 10:00:06 +0200

    interaction port definition (i'm using port and adapters architecture)

---
+++ msvc-users/src/main/java/com/jlcm/code/challenge/msvc/users/application/UsersInteractionPort.java
@@ -0,0 +1,26 @@
+package com.jlcm.code.challenge.msvc.users.application;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+import java.util.SortedMap;
+
+import com.jlcm.code.challenge.msvc.users.domain.User;
+
+public interface UsersInteractionPort {
+
+    public Collection<User> findAll();
+
+    public Optional<User> findByUsername(String username);
+
+    public User create(User user);
+
+    public Optional<User> update(String username, User user);
+
+    public Optional<User> delete(String username);
+
+    public Collection<User> generateUsers(int count);
+
+    public SortedMap<String, List<User>> findByCountry(String country);
+
+}
