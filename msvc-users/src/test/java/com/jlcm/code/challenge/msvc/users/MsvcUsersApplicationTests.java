0e54e124fa61e3d02e87e7c6346128711836eb91
joselawis <joselawis@gmail.com>
2025-06-26 09:43:26 +0200

    initialization of users microservice

---
+++ msvc-users/src/test/java/com/jlcm/code/challenge/msvc/users/MsvcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlcm.code.challenge.msvc.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
