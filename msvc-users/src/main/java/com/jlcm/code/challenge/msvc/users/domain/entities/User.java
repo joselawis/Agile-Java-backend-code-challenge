2ee69ac694f8b22f6c3840ec070185e964f07d3d
joselawis <joselawis@gmail.com>
2025-06-26 20:41:57 +0200

    bad validation corrections

--- msvc-users/src/main/java/com/jlcm/code/challenge/msvc/users/domain/entities/User.java
+++ msvc-users/src/main/java/com/jlcm/code/challenge/msvc/users/domain/entities/User.java
@@ -14,18 +14,15 @@ public class User {
     @NotBlank
     private String username;
 
-    @NotBlank
     private FullName name;
 
     @NotBlank
     @Email
     private String email;
 
-    @NotBlank
     private Gender gender;
 
     private String pictureUrl;
 
-    @NotBlank
     private Location location;
 }
