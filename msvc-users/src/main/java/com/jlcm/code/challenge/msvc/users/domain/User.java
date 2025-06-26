99b9754af4b2c91bf1536c3776710fae26302285
joselawis <joselawis@gmail.com>
2025-06-26 12:49:09 +0200

    formating user class

--- msvc-users/src/main/java/com/jlcm/code/challenge/msvc/users/domain/User.java
+++ msvc-users/src/main/java/com/jlcm/code/challenge/msvc/users/domain/User.java
@@ -1,5 +1,6 @@
 package com.jlcm.code.challenge.msvc.users.domain;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,15 +10,22 @@ import lombok.Data;
 @AllArgsConstructor
 @Builder
 public class User {
+
     @NotBlank
     private String username;
+
     @NotBlank
     private FullName name;
+
     @NotBlank
+    @Email
     private String email;
+
     @NotBlank
     private Gender gender;
-    private String picture;
+
+    private String pictureUrl;
+
     @NotBlank
     private Location location;
 }
